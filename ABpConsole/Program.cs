41b35cc2e1be908ecbbeffc2d3116513da5368ab
马志亮 <mazhiliang@benlai.com>
2022-05-26 19:28:14 +0800

    abp创建

---
+++ ABpConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
